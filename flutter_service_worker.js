38a548699eb41f661a7209f3e4ec0422f2004db4
JJJ-J7 <jojo.reg.services@gmail.com>
2025-07-18 03:17:39 +0900

    Update flutter_service_worker.js Manually

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -38,7 +38,8 @@ const CORE = ["main.dart.js",
 "index.html",
 "flutter_bootstrap.js",
 "assets/AssetManifest.bin.json",
-"assets/FontManifest.json"];
+"assets/FontManifest.json",
+"flutter.js"];
 
 // During install, the TEMP cache is populated with the application shell files.
 self.addEventListener("install", (event) => {
